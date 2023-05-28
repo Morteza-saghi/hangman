4b4e4a59f4361926dead32a11e2eb4c3e0673f5e
Mo <mort.saghi@gmail.com>
2023-05-28 19:19:33 +0330

    the project hangman done Not the best code though

---
+++ New.py
@@ -0,0 +1,59 @@
+from words import words
+from random import randint as rand
+randomNum = rand(0, len(words) - 1)
+chosenWord = words[randomNum]
+# print(chosenWord)
+temp = str()
+
+for i in chosenWord:
+    temp += "-"
+
+healthBar = 10
+chosedLetters = list()
+
+chosenWord = list(chosenWord)
+temp3 = str()
+
+
+def complition():
+    temp2 = str()
+    global temp3
+    for i in chosenWord:
+        if i in chosedLetters:
+            temp2 += i
+        else:
+            temp2 += "-"
+    print(temp2)
+    temp3 = temp2
+
+
+def gameFinished():
+    theFinish = bool()
+    for i in temp3:
+        if i == "-":
+            theFinish = False
+        else:
+            theFinish = True
+    return theFinish
+
+
+while healthBar > 0:
+    if gameFinished() == True:
+        print("You Have Won Bitch")
+        break
+    theinput = input("please enter your guess\n")
+    if theinput in chosenWord:
+        print("guesed right motherfucker\n")
+        chosedLetters.append(theinput)
+        print(f"You Still Have {healthBar} Hearts")
+        complition()
+        print(f"You Have Chosed Theese Letters {chosedLetters}")
+        print("====================")
+    else:
+        print("Not There...\nYou Better Work Bitch")
+        healthBar -= 1
+        chosedLetters.append(theinput)
+        print(f"You Still Have {healthBar} Hearts")
+        complition()
+        print(f"You Have Chosed Theese Letters {chosedLetters}")
+        print("====================")
